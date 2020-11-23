4787da9e4feebd38a3e251ea50f4c9071ba19684
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-23 11:03:19 +0330

    Added a body

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
         title: 'Simple App',
         theme: ThemeData(primaryColor: Colors.green[500]),
         home: Scaffold(
-          appBar: AppBar(title: Text("A simple app!")),
-        ));
+            appBar: AppBar(title: Text("A simple app!")),
+            body: Center(
+              child: Text("This is my app!"),
+            )));
   }
 }

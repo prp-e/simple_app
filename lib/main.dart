b75f3ed3f5d34343e1e65798671d33c38f77a009
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-23 11:15:51 +0330

    Made it a bit better

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Simple App',
-        theme: ThemeData(primaryColor: Colors.green[500]),
-        home: _HomePage();
+      title: 'Simple App',
+      theme: ThemeData(primaryColor: Colors.green[500]),
+      home: HomePage(),
+    );
   }
 }
 
 class HomePage extends StatefulWidget {
-  @override 
-  _HomePage createState() => _HomePage(); 
+  @override
+  _HomePage createState() => _HomePage();
 }
 
 class _HomePage extends State<HomePage> {
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Text from a stateful widget!"),),
+      body: Center(
+        child: Text("Text from a stateful widget!"),
+      ),
     );
   }
 }
